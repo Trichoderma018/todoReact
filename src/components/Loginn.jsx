fcdbec08bcd2e4a8c0494ca37c33a82f9cbecdf0
Juan Quesada <juan.paq72@gmail.com>
2025-03-09 21:45:37 -0600

    cambios al login y register, no funcionan

--- src/components/Loginn.jsx
+++ src/components/Loginn.jsx
@@ -2,15 +2,53 @@ import React from 'react'
 import '../styles/Loginn.css'
 
 function Loginn() {
+  const [nombreUsuario,SetNombreUsuario]=useState()
+  const [contraUsuario,SetcontraUsuario]=useState()
+  const [usuarios,SetUsuarios]=useState()
+
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+  
+    async function fetchDataUsers() {
+
+      const datos = await llamados.GetUsers()
+      
+      SetUsuarios(datos)
+    };
+    fetchDataUsers();
+  }, []); 
+
+  function nombre(evento) {
+    SetNombreUsuario(evento.target.value)
+  }
+
+  function contrasena(evento) {
+    SetcontraUsuario(evento.target.value)
+  }
+
+  function validar() {
+    console.log(nombreUsuario,contraUsuario);
+    console.log(usuarios);
+
+    const encontrado = usuarios.filter(usuario => usuario.nombre===nombreUsuario && usuario.contrasena=== contraUsuario)
+     
+    if (encontrado.length === 0) {
+      console.log("Usuario o contrasena incorrectos"); 
+    }else{
+      navigate('/Home')
+    }
+  }
   return (
     <div>
       <div className='login'>
-        <h4>Login</h4>
-        <label htmlFor="">Nombre</label>
-        <input type="text" />
-        <label htmlFor="">Contraseña</label>
-        <input type="password" />
-        <button>Iniciar Sesion</button>
+      <label htmlFor="">Nombre</label>
+      <input value={nombreUsuario} onChange={nombre} type="text" />
+      <label htmlFor="">Contraseña</label>
+      <input value={contraUsuario} onChange={contrasena} type="text" />
+      <button onClick={validar}>Iniciar</button>
+      <p>¿No tienes cuenta? <Link to="/register">Regístrate aquí</Link></p>
       </div>
     </div>
   )
--- src/components/Registerr.jsx
+++ src/components/Registerr.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+import llamados from '../services/llamados'
 import '../styles/Register.css'
 
 function Registerr() {
+  const [nombreUsuario,SetNombreUsuario]=useState()
+  const [correoUsuario,SetCorreoUsuario]=useState()
+  const [contraUsuario,SetContraUsuario]=useState()
+
+  function cargar() {
+    llamados.PostUsers(nombreUsuario,correoUsuario,contraUsuario)
+  }
+  function contrasena(evento) {
+    SetContraUsuario(evento.target.value)
+  }
+  function correo(evento) {
+    SetCorreoUsuario(evento.target.value)
+  }
+  function nombre(evento) {
+    SetNombreUsuario(evento.target.value)
+  }
   return (
     <div>
       <div className='div-register'>
-        <label htmlFor="">Nombre</label>
-        <input type="text" />
-        <label htmlFor="">Correo</label>
-        <input type="text" />
-        <label htmlFor="">Contraseña</label>
-        <input type="password" />
-        <button>Registrarse</button>
+        <label >Nombre</label>
+        <input value={nombreUsuario} onChange={nombre} type="text" />
+        <label >Correo</label>
+        <input value={correoUsuario} onChange={correo} type="text" />
+        <label >Contraseña</label>
+        <input value={contraUsuario} onChange={contrasena} type="password" />
+        <button onClick={cargar}>Registrarse</button>
+
+        <p>¿Ya tienes una cuenta? <Link to="/login">Inicia aqui</Link></p>
       </div>
     </div>
   )
